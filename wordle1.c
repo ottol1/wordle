88bdd4b52dc74e496dc07a9ad1eb8dcf78178c01
ottol1 <162608916+ottol1@users.noreply.github.com>
2024-03-07 10:01:54 -0700

    Create wordle1.c

---
+++ wordle1.c
@@ -0,0 +1,52 @@
+/*************
+*wordle proj 1
+*
+*************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void green(){
+  printf(" \e[1;32m");
+}
+
+void yellow() {
+  printf("\033[1;33m");
+}
+
+
+
+
+   int main(){
+     
+     char l1, l2, l3, l4, l5, word;
+     int tries=0;
+
+
+     printf("How to play\n");
+     printf("Each of your guesse must be a 5 letter word\n");
+     printf("The color of the tiles will change to show you how close your guess was\n");
+     printf("green- letter is in the correct spot, yellow- in the word but wrong spot, white- is not in the word\n");
+
+
+
+
+
+
+     while(tries <= 6);
+ 
+     
+   //  for(tries=o; tries<=6; tries++){
+       printf("attempt 1: %c %c %c %c %c", l1, l2, l3, l4, l5         
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
