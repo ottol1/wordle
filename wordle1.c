a4b3a92a28d89b158b8723da3ac968c98b31e5d7
Paytn-Barnette <162609096+Paytn-Barnette@users.noreply.github.com>
2024-03-19 09:19:31 -0700

    Add files via upload

--- wordle1.c
+++ wordle1.c
@@ -19,25 +19,41 @@ void yellow() {
 
    int main(){
      
-     char l1, l2, l3, l4, l5, word;
-     int tries=0;
+     char l1, l2, l3, l4, l5;
+     int x,tries=0;
 
-
+// instructions to play
      printf("How to play\n");
      printf("Each of your guesse must be a 5 letter word\n");
      printf("The color of the tiles will change to show you how close your guess was\n");
-     printf("green- letter is in the correct spot, yellow- in the word but wrong spot, white- is not in the word\n");
+     printf("green- letter is in the correct spot, yellow- in the word but wrong spot, white-is not in the word\n");
+
+     FILE* file = fopen("wordle.txt","r");
+     if (file == NULL){
+        printf("The file cannot be opened\n");
+        return 1;
+     }
+     srand(time(NULL));
+     char word[6];
+     for (x=0;x<rand()%95;x++){
+     fscanf(file,"%s",word);
+     }
+     printf("%s\n", word);
+     
+     
+     
+     
+     
 
 
 
 
 
-
-     while(tries <= 6);
+    // while(tries <= 6);
  
      
    //  for(tries=o; tries<=6; tries++){
-       printf("attempt 1: %c %c %c %c %c", l1, l2, l3, l4, l5         
+     //  printf("attempt 1: %c %c %c %c %c", l1, l2, l3, l4, l5         
 
 
 
